92bf59508cb44c29d0037955724c9a8c270feeb7
Zeal J Mayfield <zeal.j.mayfield@gmail.com>
2019-11-22 12:27:18 -0800

    Revert temporary debug code

--- public/index.php
+++ public/index.php
@@ -33,7 +33,7 @@ if (Route::exists($route)) {
   // 404
   $return = (object) [
     'error' => '404 - Page not found.',
-    'details' => $_SERVER // sprintf('No handler found for route: %s', $route),
+    'details' => sprintf('No handler found for route: %s', $route),
   ];
 }
 
