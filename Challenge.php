e2c1c60e42f7d9b20434ab1d1f978e65bc6bc5b0
Zeal J Mayfield <zeal.j.mayfield@gmail.com>
2019-11-25 13:51:51 -0800

    Delete whether pass or fail, provide ID

--- Challenge.php
+++ Challenge.php
@@ -106,12 +106,12 @@ class Challenge
     if (empty($result->id)) {
       throw new Exception('Challenge ID invalid/expired.');
     }
+    // delete after checking, challenge only good for one request
+    $statement = $db->prepare('DELETE FROM challenge WHERE id = :id');
+    $statement->execute(['id' => $id]);
     if (empty($result->pass)) {
       return false;
     }
-    // delete after checking, challenge only good for one request
-    $statement = $db->prepare('DELETE FROM challenge WHERE id = :id');
-    $statement->execute();
     return true;
   }
 
