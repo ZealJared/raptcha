1e2da3e5b40d32de0134ee775d3bf2e8ad789eb4
Zeal J Mayfield <zeal.j.mayfield@gmail.com>
2021-02-08 20:28:38 -0800

    Reorganize

--- src/DB.php
+++ src/DB.php
@@ -1,16 +1,15 @@
-<?php
-
+<?php declare(strict_types=1);
 class DB
 {
-  private static PDO|null $connection = null;
+  private static ?PDO $connection = null;
 
   public static function getConnection(): PDO
   {
     if (is_null(self::$connection)) {
-      self::$connection = new PDO(sprintf('sqlite:%s/data/db.sqlite3', __DIR__));
+      $dbPath = realpath(__DIR__ . '/../data/db.sqlite3');
+      self::$connection = new PDO($dbPath);
       self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
-
     return self::$connection;
   }
 }
---
+++ src/common.php
@@ -0,0 +1,5 @@
+<?php declare(strict_types=1);
+
+require_once __DIR__.'/../vendor/autoload.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
